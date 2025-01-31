4ffa8acbb17ffc2a6895917bca2e2dc6af95c4a1
Titouan Launay <titouan.launay@lleedpartners.com>
2025-01-31 10:34:58 +0100

    feat: support local Supabase URL configuration in development

--- apps/studio/src/utils/supabase/middleware.ts
+++ apps/studio/src/utils/supabase/middleware.ts
@@ -2,7 +2,10 @@ import { createServerClient } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
 
 export async function updateSession(request: NextRequest) {
-	if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+	if (
+		(!process.env.NEXT_PUBLIC_SUPABASE_URL && !process.env.SUPABASE_URL) ||
+		!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+	) {
 		throw new Error("Missing Supabase environment variables");
 	}
 
@@ -11,7 +14,7 @@ export async function updateSession(request: NextRequest) {
 	});
 
 	const supabase = createServerClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL,
+		(process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL) as string,
 		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 		{
 			cookies: {
--- apps/studio/src/utils/supabase/server.ts
+++ apps/studio/src/utils/supabase/server.ts
@@ -4,14 +4,17 @@ import { cookies } from "next/headers";
 export async function createClient() {
 	const cookieStore = await cookies();
 
-	if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+	if (
+		(!process.env.NEXT_PUBLIC_SUPABASE_URL && !process.env.SUPABASE_URL) ||
+		!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+	) {
 		throw new Error("Missing Supabase environment variables");
 	}
 
 	// Create a server's supabase client with newly configured cookie,
 	// which could be used to maintain user's session
 	return createServerClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL,
+		(process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL) as string,
 		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 		{
 			cookies: {
